59de564e894615de3e509ee8663fee4378539143
Mariana Felix <124410469+FelixMari@users.noreply.github.com>
2023-10-16 19:11:17 -0700

    Add files via upload

--- PARCIAL2/menu.php
+++ PARCIAL2/menu.php
@@ -17,10 +17,10 @@
             Menú
           </a>
           <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Consulta alumnos</a></li>
+            <li><a class="dropdown-item" href="#">Inicio</a></li>
             <li><a class="dropdown-item" href="#">Registra alumnos</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="#">Consulta alumnos</a></li>
           </ul>
         </li>
         <li class="nav-item">
