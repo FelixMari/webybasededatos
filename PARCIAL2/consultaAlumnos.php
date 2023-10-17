7e4135ed8c05085b6a1d63c1096db6e7ec5c9dcc
Mariana Felix <124410469+FelixMari@users.noreply.github.com>
2023-10-16 19:25:01 -0700

    Add files via upload

--- PARCIAL2/consultaAlumnos.php
+++ PARCIAL2/consultaAlumnos.php
@@ -13,6 +13,33 @@
     $sql = "SELECT * FROM alumnos";
     $datos = $conexion->query($sql); ?>
 
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <?php if($datos->num_rows > 0){ ?>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Nombre</th>
+                                <th>Número de control</th>
+                                <th>Semestre</th>
+                                <th>Edad</th>
+                                <th>Turno</th>
+                                <th>Sexo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+               <?php } else { ?>
+                <h2>No existen datos de alumnos en la base de datos</h2>
+               <?php } ?>
+            </div>
+        </div>
+    </div>
+
 <footer class="text-center"><hr>
 <span>Cetis 107 web app &copy; 2023</span>
 </footer>
